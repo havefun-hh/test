dde72d1c69cb4957beb44b997aa1d575ddf7e8a2
havefun-hh <https://github.com/havefun-hh>
2021-09-22 22:00:31 +0800

    b2 commit 4

--- demo3.py
+++ demo3.py
@@ -2,5 +2,13 @@
 GEN = 2000
 LEN = 500
 
+def add(a, b):
+    return a + b
+
+def plus(a, b):
+    return a * b
+
 if __name__ == '__main__':
     print(GEN)
+    res1 = add(1, 2)
+    res2 = plus(1, 2)
